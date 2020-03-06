037b7baeb149cc2565bbfa22014ea5db4b4f0178
safabsaid <61867761+safabsaid@users.noreply.github.com>
2020-03-06 11:55:42 +0100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-crud
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+import TransactionList from './components/TransactionList';
+
+function App() {
+  return (
+    <TransactionList />
+  );
+}
+
+export default App;
